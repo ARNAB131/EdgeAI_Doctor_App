7c5c984da1eace473bdb36f518669144a4643b34
Arnab Deb <65126698+ARNAB131@users.noreply.github.com>
2025-07-29 06:27:39 +0600

    Update init.py

--- edge_core/init.py
+++ edge_core/init.py
@@ -3,4 +3,6 @@ from .DataManager import DataManager
 from .ProductionVitalsPredictor import ProductionVitalsPredictor
 from .DigitalTwinManager import DigitalTwinManager
 from .AlertManager import AlertManager
-from .SimulatedSensors import SimulatedECGSensor, SimulatedPulseOximeter, SimulatedBloodPressureMonitor
+from .SimulatedECGSensor import SimulatedECGSensor
+from .SimulatedPulseOximeter import SimulatedPulseOximeter
+from .SimulatedBloodPressureMonitor import SimulatedBloodPressureMonitor
